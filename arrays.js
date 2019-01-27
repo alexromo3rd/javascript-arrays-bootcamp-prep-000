4bc8ae54fea7e6771cb60269171e3162dcdec011
Alex Romo <ayromo08@gmail.com>
2019-01-27 20:29:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice();
+  array = array.slice();
 }
 
 // function destructivelyRemoveElementFromEndOfArray(array) {
