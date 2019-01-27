3dd0d2a4ddb831e1f8dc0c5a5c213acf5783ab4d
Alex Romo <ayromo08@gmail.com>
2019-01-27 20:08:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,10 +10,10 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  array.push(element);
-  return array;
+  return [...array, element];
 }
 
-function addElementToEndOfArray(array, element) {
-  return [...array, element];
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.push(element);
+  return array;
 }
